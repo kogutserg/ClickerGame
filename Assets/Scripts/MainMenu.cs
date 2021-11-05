f5d830d35d510dc180b815dfa30ab1a6b930863e
Sergii Kogut <skogut@playtika.com>
2021-11-05 18:03:59 +0200

    Added Main and Achivements scenes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,15 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] int money;
+    public int total_money;
     public Text moneyText;
+
+    private void Start()
+    {
+        money = PlayerPrefs.GetInt("money");
+        total_money = PlayerPrefs.GetInt("total_money");
+    }
 
     public void ButtonClick()
     {
         money++;
+        total_money++;
+        PlayerPrefs.SetInt("money", money);
+        PlayerPrefs.SetInt("total_money", total_money);
+    }
+
+    public void ToAchivements()
+    {
+        SceneManager.LoadScene(1);
     }
 
     void Update()
