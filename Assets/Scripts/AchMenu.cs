77ba7470aa70098f0c67718d7ad1ac41843ec989
Sergii Kogut <skogut@playtika.com>
2021-11-10 09:44:00 +0200

    Changed Achivements

--- Assets/Scripts/AchMenu.cs
+++ Assets/Scripts/AchMenu.cs
@@ -8,25 +8,79 @@ public class AchMenu : MonoBehaviour
 {
     public int money;
     public int total_money;
-    [SerializeField] Button firstAch;
     [SerializeField] bool isFirst;
 
-    private void OnMouseUpAsButton()
-    {
+    public string[] arrayTitles;
+    public Sprite[] arraySprites;
+    public GameObject button;
+    public GameObject content;
+
+
+    private List<GameObject> list = new List<GameObject>();
+    private VerticalLayoutGroup _group;
         
-    }
     void Start()
     {
         money = PlayerPrefs.GetInt("money");
         total_money = PlayerPrefs.GetInt("total_money");
         isFirst = PlayerPrefs.GetInt("isFirst") == 1 ? true : false;
-        if (total_money >= 10 && !isFirst == false)
+
+        RectTransform rectT = content.GetComponent<RectTransform>();
+        rectT.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+        _group = GetComponent<VerticalLayoutGroup>();
+        setAchievs();
+
+        if (isFirst)
         {
-            firstAch.interactable = true;
-        } else
-        {
-            firstAch.interactable = false;
             StartCoroutine(IdleFarm());
+        }
+    }
+
+    private void RemoveList()
+    {
+        foreach (var elem in list)
+        {
+            Destroy(elem);
+        }
+        list.Clear();
+    }
+
+    void setAchievs()
+    {
+        RectTransform rectT = content.GetComponent<RectTransform>();
+        rectT.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+        RemoveList();
+        if (arrayTitles.Length > 0)
+        {
+            var pr1 = Instantiate(button, transform);
+            var h = pr1.GetComponent<RectTransform>().rect.height;
+            var tr = GetComponent<RectTransform>();
+            tr.sizeDelta = new Vector2(tr.rect.width, h * arrayTitles.Length);
+            Destroy(pr1);
+            for(var i = 0; i < arrayTitles.Length; i++)
+            {
+                var pr = Instantiate(button, transform);
+                pr.GetComponentInChildren<Text>().text = arrayTitles[i];
+                pr.GetComponentsInChildren<Image>()[1].sprite = arraySprites[i];
+                var i1 = i;
+                pr.GetComponent<Button>().onClick.AddListener(() => GetAchievement(i1));
+                list.Add(pr);
+            }
+        }
+    }
+
+    void GetAchievement(int id)
+    {
+        switch(id)
+        {
+            case 0:
+                Debug.Log(id);
+                break;
+            case 1:
+                Debug.Log(id);
+                money += 10;
+                PlayerPrefs.SetInt("money", money);
+                break;
         }
     }
 
